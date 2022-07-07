25462e359b599b6286a61f67aa0367c2c8ec8e88
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-07-08 00:18:27 +0100

    🔨

--- user/queue/queue.go
+++ user/queue/queue.go
@@ -7,19 +7,22 @@ import (
 )
 
 func Connect() *amqp.Channel {
+
 	dsn := "amqp://admin:admin@localhost:5672/"
 	conn, err := amqp.Dial(dsn)
 	if err != nil {
 		panic(err)
 	}
+
 	ch, err := conn.Channel()
 	if err != nil {
 		panic(err)
 	}
+
 	return ch
 }
 
-func Producer(payload []byte, exchange string, routingKey string, ch *amqp.Channel) {
+func UserMarkVote(payload []byte, exchange string, routingKey string, ch *amqp.Channel) {
 
 	err := ch.Publish(
 		exchange,
@@ -36,40 +39,4 @@ func Producer(payload []byte, exchange string, routingKey string, ch *amqp.Chann
 	}
 
 	fmt.Println("Message sent")
-}
-
-func Consumer(queue string, ch *amqp.Channel, in chan []byte) {
-
-	q, err := ch.QueueDeclare(
-		queue,
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	msgs, err := ch.Consume(
-		q.Name,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	go func() {
-		for m := range msgs {
-			in <- []byte(m.Body)
-		}
-		close(in)
-	}()
 }
