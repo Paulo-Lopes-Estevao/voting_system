66e6ddb6dd19503e983b8a75967f187d39bb0689
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-07-08 01:30:07 +0100

    🔨

--- user/route/user_route.go
+++ user/route/user_route.go
@@ -1,1 +1,38 @@
 package route
+
+import (
+	"gomq/user/controller"
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+)
+
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func UserRoute(e *echo.Echo) *echo.Echo {
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("user/template/*.html")),
+	}
+
+	e.Renderer = renderer
+
+	e.GET("/", func(c echo.Context) error {
+		return controller.HomePageVoteController(c)
+	})
+
+	e.POST("/uservote", func(c echo.Context) error {
+		return controller.HomePageVoteController(c)
+	})
+
+	return e
+
+}
