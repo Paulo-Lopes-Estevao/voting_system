4c9f7f13d96f479269e5b17718f237a09e143b9b
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-07-10 06:21:09 +0100

    🔨

--- vote/route/vote_route.go
+++ vote/route/vote_route.go
@@ -15,10 +15,14 @@ type TemplateRenderer struct {
 // Render renders a template document
 func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-func VoteRoute(e *echo.Echo) *echo.Echo {
+func VoteRoute(e *echo.Echo, IvoteController controller.IVoteController) *echo.Echo {
 	renderer := &TemplateRenderer{
 		templates: template.Must(template.ParseGlob("vote/template/*.html")),
 	}
@@ -26,7 +30,7 @@ func VoteRoute(e *echo.Echo) *echo.Echo {
 	e.Renderer = renderer
 
 	e.GET("/vote", func(c echo.Context) error {
-		return controller.ShowTheVotesController(c)
+		return IvoteController.ShowTheVotesController(c)
 	})
 
 	return e
