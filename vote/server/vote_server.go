90a9b66253a162c96f73d65562f7dccec06fbd18
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-07-10 06:20:59 +0100

    🔨

--- vote/server/vote_server.go
+++ vote/server/vote_server.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"gomq/vote/controller"
+	"gomq/vote/factory"
+	"gomq/vote/infra/db"
 	"gomq/vote/route"
 
 	"github.com/labstack/echo/v4"
@@ -9,7 +12,10 @@ import (
 func VoteServer() {
 	e := echo.New()
 
-	route.VoteRoute(e)
+	conn := db.ConnectDB()
+	voteUsecaseFactory := factory.VoteUsecaseFactory(conn)
+	voteController := controller.New(&voteUsecaseFactory)
+	route.VoteRoute(e, voteController)
 
 	e.Logger.Fatal(e.Start(":8002"))
 }
