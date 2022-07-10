84d5b5951d366bb9c37efd3c8555fa3779312bba
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-07-10 06:19:27 +0100

    ✨

---
+++ vote/usecase/vote_usecase.go
@@ -0,0 +1,32 @@
+package usecase
+
+import (
+	"gomq/vote/entities"
+	"gomq/vote/entities/repository"
+)
+
+type IVoteUseCase interface {
+	RegisterVotes(name, vote string) (*entities.Vote, error)
+}
+
+type VoteUseCase struct {
+	Irespositoy repository.IVoteRepository
+}
+
+func New(Irespo repository.IVoteRepository) IVoteUseCase {
+	return &VoteUseCase{
+		Irespositoy: Irespo,
+	}
+}
+
+func (v *VoteUseCase) RegisterVotes(name, vote string) (*entities.Vote, error) {
+	votes := &entities.Vote{
+		Name: name,
+		Vote: vote,
+	}
+	err := v.Irespositoy.AddVotes(votes)
+	if err != nil {
+		return nil, err
+	}
+	return votes, nil
+}
