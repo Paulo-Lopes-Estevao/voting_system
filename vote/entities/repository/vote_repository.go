b38991ea9dd0c24fa1ea022be9cf3b38f0711251
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-07-10 09:26:47 +0100

    🔨

--- vote/entities/repository/vote_repository.go
+++ vote/entities/repository/vote_repository.go
@@ -4,5 +4,5 @@ import "gomq/vote/entities"
 
 type IVoteRepository interface {
 	AddVotes(votes *entities.Vote) error
-	ShowVotes() (*entities.Vote, error)
+	ShowVotes() ([]*entities.Vote, error)
 }
