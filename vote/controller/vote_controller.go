ea8c93db5aa0f37fdc3bd0b866f77bef88d65785
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-07-10 06:18:32 +0100

    🔨

---
+++ vote/controller/vote_controller.go
@@ -0,0 +1,45 @@
+package controller
+
+import (
+	"fmt"
+	"gomq/vote/dto"
+	"gomq/vote/queue"
+	"gomq/vote/usecase"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type IVoteController interface {
+	ShowTheVotesController(c echo.Context) error
+}
+
+type voteController struct {
+	Ivoteusecase usecase.IVoteUseCase
+}
+
+func New(Ivoteusecase usecase.IVoteUseCase) IVoteController {
+	return &voteController{
+		Ivoteusecase: Ivoteusecase,
+	}
+}
+
+func (vc *voteController) ShowTheVotesController(c echo.Context) error {
+	in := make(chan []byte)
+
+	connection := queue.Connect()
+	go queue.ShowVotes("vote_ex", "vote_rk", "direct", connection, in)
+
+	var v dto.Vote
+	go func() {
+		for payload := range in {
+			v.ParseJson(payload)
+			vc.Ivoteusecase.RegisterVotes(v.Name, v.Vote)
+			fmt.Println(v)
+		}
+	}()
+
+	return c.Render(http.StatusOK, "votes.html", map[string]interface{}{
+		"data": "",
+	})
+}
